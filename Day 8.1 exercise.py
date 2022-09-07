98a3695df67a0a0f8d5c89419457dbe5f6a8019c
sristiM123 <73756057+sristiM123@users.noreply.github.com>
2022-09-08 01:13:55 +0530

    Create Day 8.1 exercise.py

---
+++ Day 8.1 exercise.py
@@ -0,0 +1,18 @@
+#Write your code below this line 👇
+import math
+def paint_calc(height, width, cover): 
+  area= height * width
+  number_of_cans= math.ceil(area/cover)
+  print(number_of_cans)
+  
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
+
